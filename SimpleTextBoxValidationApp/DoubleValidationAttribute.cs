18e83475389157c64903cd85079148d1b2babfbe
CresBloomoon <metroidm2421@gmail.com>
2024-02-03 18:56:00 +0900

    プロジェクト ファイルを追加します。

---
+++ SimpleTextBoxValidationApp/DoubleValidationAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleTextBoxValidationApp {
+    public class DoubleValidationAttribute : ValidationAttribute {
+        public override bool IsValid(object value) => double.TryParse(value.ToString(), out var _);
+    }
+}
---
+++ SimpleTextBoxValidationApp/IntValidationAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleTextBoxValidationApp {
+    public class IntValidationAttribute : ValidationAttribute {
+        public override bool IsValid(object value) => int.TryParse(value.ToString(), out var _);
+    }
+}
